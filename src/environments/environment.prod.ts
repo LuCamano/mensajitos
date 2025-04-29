87b7e92d9552fd61b891f33f000828be73bd47c8
Luciano <lu.camano@duocuc.cl>
2025-04-29 18:04:13 -0400

    feat: integrate Firebase into the Ionic application
    - Added @angular/fire dependency to package.json for Firebase integration.
    - Updated app.module.ts to initialize Firebase and provide Auth and Firestore services.
    - Created a new FireService for future Firestore operations.
    - Updated environment files to include Firebase configuration for both development and production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseconfig: { 
+    projectId: "mensajitos", 
+    appId: "1:11196474795:web:dd2c61063a39ea52ef5eee", 
+    storageBucket: "mensajitos.firebasestorage.app", 
+    apiKey: "AIzaSyAFPoxaNTI_5AIvktxei7LaAmxQh1mtX7Y", 
+    authDomain: "mensajitos-fb492.firebaseapp.com", 
+    messagingSenderId: "11196474795" 
+  }
 };
