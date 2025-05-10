74d61de480ce8530bd265050795d748a0ad1cefa
Luciano <lu.camano@duocuc.cl>
2025-05-09 22:17:49 -0400

    feat: implementar servicio de autenticación y utilidades para manejo de usuarios

--- src/app/services/fire.service.ts
+++ src/app/services/fire.service.ts
@@ -1,9 +1,54 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { AngularFirestore } from "@angular/fire/compat/firestore";
+import { getDoc, setDoc, doc, addDoc, collection, collectionData, query, where } from "@angular/fire/firestore";
+import { User } from '../models/usuario.models';
+import { UtilsService } from './utils.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FireService {
+  // Inyeccion de dependencias
+  private ngFireAuth = inject(AngularFireAuth);
+  private ngFirestore = inject(AngularFirestore);
+  private utils = inject(UtilsService);
 
-  constructor() { }
+  async signUp(email: string, password: string, name: string){
+    const userCredential = await this.ngFireAuth.createUserWithEmailAndPassword(email, password);
+    const user: User = {
+      uid: userCredential.user!.uid,
+      email: email,
+      displayName: name
+    };
+    this.setDocument(`users/${user.uid}`, user);
+    return user;
+  }
+
+  // Metodo para iniciar sesion
+  async signIn(email: string, password: string){
+    const userCredential = await this.ngFireAuth.signInWithEmailAndPassword(email, password);
+    const user = await this.getDocument(`users/${userCredential.user!.uid}`) as User;
+    this.utils.saveInLocalStorage('user', user);
+    return user;
+  }
+
+  resetPassword(email: string){
+    return this.ngFireAuth.sendPasswordResetEmail(email);
+  }
+
+  // Setea un documento en la ruta especificada (esta ruta debe contener la clave primaria del documento ya que no se genera automaticamente)
+  // Si el documento no existe, lo crea. Si existe, lo actualiza.
+  setDocument(path:string, data:any) {
+    return setDoc(doc(this.ngFirestore.firestore, path), data);
+  }
+
+  // Crea un documento en la ruta especificada (esta ruta no debe contener la clave primaria del documento ya que se genera automaticamente)
+  addDocument(path: string, data: any){
+    return addDoc(collection(this.ngFirestore.firestore, path), data);
+  }
+
+  async getDocument(path: string){
+    return (await getDoc(doc(this.ngFirestore.firestore, path))).data();
+  }
 }
---
+++ src/app/services/utils.service.ts
@@ -0,0 +1,52 @@
+import { inject, Injectable } from '@angular/core';
+import { NavigationExtras } from '@angular/router';
+import { AlertController, AlertOptions, LoadingController, NavController, ToastController, ToastOptions } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilsService {
+  private navCtrl = inject(NavController);
+  private toastController = inject(ToastController);
+  private loadingController = inject(LoadingController);
+  private alertController = inject(AlertController);
+
+  saveInLocalStorage(key: string, value: any){
+    return localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  getFromLocalStorage(key: string){
+    return JSON.parse(localStorage.getItem(key)!);
+  }
+
+  async presentToast(opts: ToastOptions){
+    const toast = await this.toastController.create(opts);
+    toast.present();
+    return toast;
+  }
+
+  presentLoading(){
+    return this.loadingController.create({
+      message: 'Cargando...',
+      spinner: 'crescent'
+    });
+  }
+
+  async presentAlert(opts: AlertOptions){
+    const alert = await this.alertController.create(opts);
+    alert.present();
+    return alert;
+  }
+
+  navigateForwardTo(route: string, extras?: NavigationExtras){
+    this.navCtrl.navigateForward(route, extras);
+  }
+
+  navigateBack(){
+    this.navCtrl.back();
+  }
+
+  navigateRoot(route: string, extras?: NavigationExtras){
+    this.navCtrl.navigateRoot(route, extras);
+  }
+}
