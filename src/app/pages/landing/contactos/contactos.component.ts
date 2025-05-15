f24444f82f7cda6f728ef61034e47d43d10f1753
Paulsalinas1 <paul.salinasc@duocuc.cl>
2025-05-15 17:00:05 -0400

    Visualizacion de contactos y un poco de como se agregan

--- src/app/pages/landing/contactos/contactos.component.ts
+++ src/app/pages/landing/contactos/contactos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonModal, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-contactos',
@@ -7,9 +8,83 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class ContactosComponent  implements OnInit {
+  contactos = [
+    {
+      email: 'ElCid@example.com',
+      uid: '122',
+      nombre : 'Cid'
+    },
+    {
+      email: 'paul@example.com',
+      uid: '123',
+      nombre : 'Paul'
+    }  
+  ]
+  open_new_contact = false;
 
-  constructor() { }
+  @ViewChild('new_contact', { static: true }) modal!: IonModal;
+
+  newEmail = '';
+  newNombre = '';
+  
+  constructor(private toastCtrl: ToastController ) { }
 
   ngOnInit() {}
+  
+  toggleModal() {
+    this.open_new_contact = !this.open_new_contact;
+    this.newEmail = '';
+    this.newNombre = '';
+    
+  }
+
+  isEmailValid(email: string): boolean {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
+
+  async showToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2000,
+      color: 'danger',
+      position: 'bottom',
+    });
+    toast.present();
+  }
+
+  async addContact() {
+    if (!this.newEmail || !this.newNombre) {
+      this.showToast('Por favor, complete todos los campos.');
+      return;
+    }
+
+    if (!this.isEmailValid(this.newEmail)) {
+      this.showToast('Ingrese un correo electrónico válido.');
+      return;
+    }
+
+    // Si todo está bien, agregamos el contacto
+    this.contactos.push({ email: this.newEmail, uid: '', nombre: this.newNombre });
+
+    // Limpiamos los campos para el próximo contacto
+    this.newEmail = '';
+    this.newNombre = '';
+
+    // Cerramos el modal
+    this.toggleModal();
+  }
+
+
+  cancel() {
+    this.modal.dismiss();
+  }
+
+  onWillDismiss(event: Event) {
+    this.open_new_contact = false;
+    this.newEmail = '';
+    this.newNombre = '';
+    
+  }
 
 }
