fc8b148ee49de04ec0cac5e0bebd8d0eeaa5643f
SeSMP <se.santamaria@duocuc.cl>
2025-05-29 13:40:55 -0400

    bloqueo de contactos

--- src/app/models/contacto.models.ts
+++ src/app/models/contacto.models.ts
@@ -1,4 +1,5 @@
 export interface Contacto {
   uid?: string;  
   email: string;
+  bloqueado?: boolean;
 }
--- src/app/pages/landing/contactos/contactos.component.ts
+++ src/app/pages/landing/contactos/contactos.component.ts
@@ -12,20 +12,25 @@ export class ContactosComponent  implements OnInit {
     {
       email: 'ElCid@example.com',
       uid: '122',
-      nombre : 'Cid'
+      nombre : 'Cid',
+      bloqueado: false
     },
     {
       email: 'paul@example.com',
       uid: '123',
-      nombre : 'Paul'
+      nombre : 'Paul',
+      bloqueado: false
     },
     {
       email: 'c@example.com',
       uid: '12345523541',
-      nombre : 'seba'
+      nombre : 'seba',
+      bloqueado: false
     }   
   ]
   open_new_contact = false;
+  open_contact_options = false;
+  selectedContact: any = null;
 
   @ViewChild('new_contact', { static: true }) modal!: IonModal;
 
@@ -64,7 +69,7 @@ export class ContactosComponent  implements OnInit {
     }
 
     //agrega el contacto
-    this.contactos.push({ email: this.newEmail, uid: '', nombre: 'usuarioXD' });
+    this.contactos.push({ email: this.newEmail, uid: '', nombre: 'usuarioXD', bloqueado: false });
 
     // Limpiar
     this.newEmail = '';
@@ -83,4 +88,20 @@ export class ContactosComponent  implements OnInit {
     this.newEmail = '';
   }
 
+  openContactOptions(contacto: any, event: Event) {
+  event.stopPropagation(); // Evita que se dispare el click del ion-item
+  this.selectedContact = contacto;
+  this.open_contact_options = true;
+}
+
+  closeContactOptions() {
+  this.open_contact_options = false;
+  this.selectedContact = null;
+}
+
+  blockContact(contacto: any) {
+  contacto.bloqueado = true;
+  this.showToast(`Contacto ${contacto.nombre} bloqueado.`);
+  this.closeContactOptions();
+}
 }
