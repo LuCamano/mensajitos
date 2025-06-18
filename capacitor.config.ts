be23a2719334e9fd0037144b5b004dfd77bac2fb
Luciano <lu.camano@duocuc.cl>
2025-06-18 11:07:07 -0400

    Agregar configuración de plugins para notificaciones push en capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mensajitos.app',
   appName: 'mensajitos',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
+  },
 };
 
 export default config;
