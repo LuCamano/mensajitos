b45e4d8754c65a3d8a2ec219aa7b046d1e0f551a
Luciano <lu.camano@duocuc.cl>
2025-07-03 19:53:57 -0400

    agregado ReactiveFormsModule y mejorada la gestión del perfil de usuario con un formulario reactivo

--- src/app/pages/landing/perfil/perfil.component.ts
+++ src/app/pages/landing/perfil/perfil.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IonModal } from '@ionic/angular';
+import { IonModalCustomEvent,OverlayEventDetail } from '@ionic/core';
+import { User } from 'src/app/models/usuario.models';
+import { FireService } from 'src/app/services/fire.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-perfil',
@@ -7,15 +13,67 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class PerfilComponent implements OnInit {
+  // Inyecciones
+  private fireSvc = inject(FireService);
+  private utils = inject(UtilsService);
 
-  user: string = '';
-  email: string = '';
+  @ViewChild(IonModal) 
+  modal!: IonModal;
 
-  constructor() { }
+  editForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
 
+  currentUser!: User;
+  
   ngOnInit() {
-    const usuario = JSON.parse(localStorage.getItem('user') || '{}');
-    this.user = usuario.displayName || 'Usuario';
-    this.email = usuario.email || '';
+    this.getUserData();
+  }
+
+  confirm() {
+    this.modal.dismiss(null, 'confirm')
+  }
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirmEdit(event: IonModalCustomEvent<OverlayEventDetail<any>>) {
+    if (event.detail.role === 'confirm' && this.editForm.valid) {
+      this.updateProfile();
+    }
+  }
+
+  getUserData() {
+    this.currentUser = this.utils.getFromLocalStorage('user');
+    this.editForm.patchValue({
+      username: this.currentUser.displayName,
+      email: this.currentUser.email
+    });
+  }
+
+  async updateProfile() {
+    const updatedUser: User = {...this.currentUser}
+    updatedUser.displayName = this.editForm.value.username || this.currentUser.displayName;
+    const loading = await this.utils.presentLoading();
+    try {
+      await this.fireSvc.setDocument(`users/${this.currentUser.uid}`, updatedUser);
+      this.utils.saveInLocalStorage('user', updatedUser);
+      this.utils.presentToast({
+        message: 'Perfil actualizado correctamente',
+        duration: 2000,
+        color: 'success'
+      });
+      this.getUserData();
+    } catch (error) {
+      this.utils.presentToast({
+        message: 'Error al actualizar el perfil',
+        duration: 2000,
+        color: 'danger'
+      });
+    } finally {
+      loading.dismiss();
+    }
   }
 }
